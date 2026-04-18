ff7523be7b4149a257d24e2a1c595ed65db4c637
jojopirker <j.pirker@gmail.com>
2026-04-18 13:38:23 +0200

    adds favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      ],
+    },
+  },
   modules: ['nuxt-auth-utils'],
   compatibilityDate: '2025-07-15',
   css: ['~/assets/css/main.css'],
